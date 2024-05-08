1446250c9724e5b0c25f775a5346b32dd2c54a04
Your Name <Heracleioncapital@gmail.com>
2024-05-08 03:24:27 +0300

    Socials update

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,34 @@
+// links
+import Link from "next/link";
+
+// icons
+import {
+  RiInstagramLine, 
+  RiThreadsLine,
+  RiTwitterLine, 
+  RiTiktokLine,
+} from 'react-icons/ri';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+  <div className='flex items-center gap-x-5 text-lg'>
+    <Link href={''} className='hover:text-accent transition-all duration-300' >
+      <RiInstagramLine/>  
+    </Link>
+
+    <Link href={''} className='hover:text-accent transition-all duration-300' >
+      <RiThreadsLine/>  
+    </Link>
+
+    <Link href={''} className='hover:text-accent transition-all duration-300' >
+      <RiTwitterLine/>  
+    </Link>
+
+    <Link href={''} className='hover:text-accent transition-all duration-300' >
+      <RiTiktokLine/>  
+    </Link>
+  </div>
+  );
 };
 
 export default Socials;
